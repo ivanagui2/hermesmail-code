28ec45a2f52f9f61a9a8362e683a4391325bd92c
prickett <kg4ygs@gmail.com>
2018-07-04 08:57:38 +0100

    Removing references to the Paige Html editor used by the previous versions of Eudora.

--- Eudora71/Eudora/MoreHelpWnd.h
+++ Eudora71/Eudora/MoreHelpWnd.h
@@ -21,6 +21,43 @@ DAMAGE. */
 
 //
 
+/*
+HERMES MESSENGER SOFTWARE LICENSE AGREEMENT | Hermes Messenger Client Source Code
+Copyright (c) 2018, Hermes Messenger Development Team. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, 
+are permitted (subject to the limitations in the disclaimer below) provided that 
+the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list 
+of conditions and the following disclaimer.
+
+Redistributions in binary form must reproduce the above copyright notice, this 
+list of conditions and the following disclaimer in the documentation and/or 
+other materials provided with the distribution.
+
+Neither the name of Hermes Messenger nor the names of its contributors
+may be used to endorse or promote products derived from this software without 
+specific prior written permission.
+
+NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY’S PATENT RIGHTS ARE GRANTED BY THIS 
+LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+“AS IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE 
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+File revised by Jeff Prickett (kg4ygs@gmail.com) on July 4, 2018
+    Removing references to Paige library dependency.
+
+*/
+
+
 #ifndef __MOREHELPWINDOW_H_
 #define __MOREHELPWINDOW_H_
 
@@ -29,7 +66,7 @@ DAMAGE. */
 	#error include 'stdafx.h' before including this file for PCH
 #endif
 
-#include "PaigeEdtView.h"
+#include "HtmlEdtView.h"
 #include "mdidrag.h"
 #include "doc.h"
 
@@ -38,19 +75,14 @@ DAMAGE. */
 // CMoreHelpPaigeView: Paige view for More Help window
 //////////////////////////////////////////////////////////////////////
 
-class CMoreHelpPaigeView : public CPaigeEdtView
+class CMoreHelpHtmlView : public CHtmlEdtView
 {
-	DECLARE_DYNCREATE(CMoreHelpPaigeView)
+	DECLARE_DYNCREATE(CMoreHelpHtmlView)
 
 public:
-	CMoreHelpPaigeView();
-	virtual ~CMoreHelpPaigeView();
+	CMoreHelpHtmlView();
+	virtual ~CMoreHtmlView();
 
-	virtual void			Clear();
-	virtual void			ResetCaret();
-	virtual void			HideCaret();
-
-	virtual void			OnInitialUpdate();
 };
 
 

3804ae9f5493d4a3f7e09b06e658d76c36e07c97
prickett <kg4ygs@gmail.com>
2018-07-04 17:13:35 +0100

    Removed DisplayFeatureNotInFreeMessage because Hermes is not shareware. Sanitized all references to Qualcomm's Shareware Manager from the LinkHistoryList.cpp file.

--- Eudora71/Eudora/LinkHistoryList.cpp
+++ Eudora71/Eudora/LinkHistoryList.cpp
@@ -23,7 +23,43 @@ INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT
 NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH 
 DAMAGE. */
 
+/*
 
+HERMES MESSENGER SOFTWARE LICENSE AGREEMENT | Hermes Messenger Client Source Code
+Copyright (c) 2018, Hermes Messenger Development Team. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, 
+are permitted (subject to the limitations in the disclaimer below) provided that 
+the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list 
+of conditions and the following disclaimer.
+
+Redistributions in binary form must reproduce the above copyright notice, this 
+list of conditions and the following disclaimer in the documentation and/or 
+other materials provided with the distribution.
+
+Neither the name of Hermes Messenger nor the names of its contributors
+may be used to endorse or promote products derived from this software without 
+specific prior written permission.
+
+NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY’S PATENT RIGHTS ARE GRANTED BY THIS 
+LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+“AS IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE 
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+File revised by Jeff Prickett (kg4ygs@gmail.com) on July 4, 2018
+    Deleted FeatureNotInFreeMessage procedure because Hermes is not shareware.
+    Sanitized references to Qualcomm's Shreware Manager from the source file.
+
+*/
 
 #include "stdafx.h"
 
@@ -33,8 +69,6 @@ DAMAGE. */
 
 #include "guiutils.h"
 #include "resource.h"
-
-#include "QCSharewareManager.h"
 
 #include "DebugNewHelpers.h"
 
@@ -307,19 +341,8 @@ int CLinkHistoryList::OnCreate(LPCREATESTRUCT lpcs)
 			m_bDrawTypeIcon = false;
 	}
 	
-	QCSharewareManager *	pSWM = GetSharewareManager();
-	if (pSWM != NULL)
-		pSWM->Register(this);
-
-	if ( UsingFullFeatureSet() )
-	{
-		//	Notify the Link History Manager that we're ready for it to fill us with URLs
-		LinkHistoryManager::Instance()->NotifyLHViewReady();
-	}
-	else
-	{
-		DisplayFeatureNotInFreeMessage();
-	}
+	//	Notify the Link History Manager that we're ready for it to fill us with URLs
+	LinkHistoryManager::Instance()->NotifyLHViewReady();
 
 	return 0;
 }
@@ -361,27 +384,7 @@ void CLinkHistoryList::OnDestroy()
 		}
 	}
 
-	QCSharewareManager *	pSWM = GetSharewareManager();
-	if (pSWM != NULL)
-		pSWM->UnRegister(this);
-
 	CListView::OnDestroy();
-}
-
-
-// ---------------------------------------------------------------------------
-//		* DisplayFeatureNotInFreeMessage							[Public]
-// ---------------------------------------------------------------------------
-//	Delete any items in the list and display the light message instead.
-
-void CLinkHistoryList::DisplayFeatureNotInFreeMessage()
-{
-	//	Remove all the items, and display the light message.
-	//	NOTE: Must be careful to never use lvi.lParam as a LPUrlItemData
-	//		  when UsingFullFeatureSet() returns false.
-	Invalidate();
-	GetListCtrl().DeleteAllItems();
-	GetListCtrl().InsertItem( 0, CRString(IDS_ERR_FEATURE_NOT_IN_FREE) );
 }
 
 

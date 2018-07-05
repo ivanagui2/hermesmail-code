3c8ff95b2906c7508b7a86a31cd6819f77e31c9c
prickett <kg4ygs@gmail.com>
2018-07-05 01:26:47 +0100

    Removed a couple of conditional DEBUG code sections from NicknamesWazooWnd.cpp

--- Eudora71/Eudora/NicknamesWazooWnd.cpp
+++ Eudora71/Eudora/NicknamesWazooWnd.cpp
@@ -1,3 +1,59 @@
+/*
+
+/* Copyright (c) 2016, Computer History Museum 
+All rights reserved. 
+Redistribution and use in source and binary forms, with or without modification, are permitted (subject to 
+the limitations in the disclaimer below) provided that the following conditions are met: 
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer. 
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following 
+   disclaimer in the documentation and/or other materials provided with the distribution. 
+ * Neither the name of Computer History Museum nor the names of its contributors may be used to endorse or promote products 
+   derived from this software without specific prior written permission. 
+NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE 
+COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
+HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH 
+DAMAGE. */
+
+/*
+
+HERMES MESSENGER SOFTWARE LICENSE AGREEMENT | Hermes Messenger Client Source Code
+Copyright (c) 2018, Hermes Messenger Development Team. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, 
+are permitted (subject to the limitations in the disclaimer below) provided that 
+the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list 
+of conditions and the following disclaimer.
+
+Redistributions in binary form must reproduce the above copyright notice, this 
+list of conditions and the following disclaimer in the documentation and/or 
+other materials provided with the distribution.
+
+Neither the name of Hermes Messenger nor the names of its contributors
+may be used to endorse or promote products derived from this software without 
+specific prior written permission.
+
+NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY’S PATENT RIGHTS ARE GRANTED BY THIS 
+LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+“AS IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE 
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+File modified by Jeff Prickett (kg4ygs@gmail.com) on July 4, 2017
+    Removed a couple of conditional DEBUG code sections
+*/    
+
 // NicknamesWazooWnd.cpp : implementation file
 //
 // CNicknamesWazooWnd
@@ -157,20 +213,6 @@ void CNicknamesWazooWnd::OnSize(UINT nType, int cx, int cy)
 ////////////////////////////////////////////////////////////////////////
 void CNicknamesWazooWnd::OnGetMinMaxInfo(MINMAXINFO FAR* lpMMI) 
 {
-#ifdef _DEBUG
-	CString msg;
-	msg.Format("CNicknamesWazooWnd::OnGetMinMaxInfo: "
-				"maxSize(%d,%d) "
-				"maxPos(%d,%d) "
-				"minTrack(%d,%d) "
-				"maxTrack(%d,%d)\n",
-				lpMMI->ptMaxSize.x, lpMMI->ptMaxSize.y,
-				lpMMI->ptMaxPosition.x, lpMMI->ptMaxPosition.y,
-				lpMMI->ptMinTrackSize.x, lpMMI->ptMinTrackSize.y,
-				lpMMI->ptMaxTrackSize.x, lpMMI->ptMaxTrackSize.y);
-	//TRACE0(msg);
-#endif // _DEBUG
-
 	lpMMI->ptMinTrackSize.x = 140;		//FORNOW, hardcoded
 	lpMMI->ptMinTrackSize.y = 240;		//FORNOW, hardcoded
 
@@ -346,15 +388,6 @@ BOOL CNicknamesWazooWnd::CreateViews()
 		{
 			return FALSE;
 		}
-
-#ifdef _DEBUG
-		{
-			// FORNOW, take a looksee at the RHS view
-			CView* pViewRight = (CView *) m_wndSplitter.GetPane(0, 1);
-			ASSERT(pViewRight);
-			ASSERT_KINDOF(CView, pViewRight);
-		}
-#endif // _DEBUG
 
 		CRect rectClient;
 		// I know i could have just taken the ClientRect for myself(i.e 'this'), but then that would be (0,0,0,0) when 'this' is in the process of creation

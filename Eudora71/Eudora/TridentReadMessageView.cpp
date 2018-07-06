54484f7f3641d8c577fccefa6271872f4d2d2631
prickett <kg4ygs@gmail.com>
2018-07-06 05:05:37 +0100

    Removed references to Qualcomm's Shareware Manager from TridentReadMessageView.cpp

--- Eudora71/Eudora/TridentReadMessageView.cpp
+++ Eudora71/Eudora/TridentReadMessageView.cpp
@@ -19,6 +19,42 @@ INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT
 NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH 
 DAMAGE. */
 
+/*
+
+HERMES MESSENGER SOFTWARE LICENSE AGREEMENT | Hermes Messenger Client Source Code
+Copyright (c) 2018, Hermes Messenger Development Team. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, 
+are permitted (subject to the limitations in the disclaimer below) provided that 
+the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list 
+of conditions and the following disclaimer.
+
+Redistributions in binary form must reproduce the above copyright notice, this 
+list of conditions and the following disclaimer in the documentation and/or 
+other materials provided with the distribution.
+
+Neither the name of Hermes Messenger nor the names of its contributors
+may be used to endorse or promote products derived from this software without 
+specific prior written permission.
+
+NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY’S PATENT RIGHTS ARE GRANTED BY THIS 
+LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+“AS IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE 
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+File revised by Jeff Prickett (kg4ygs@gmail.com) on July 6, 2018
+    Removed references to Qualcomm's Shareware Manager.
+*/    
+
 //
 
 #include "stdafx.h"
@@ -52,8 +88,6 @@ DAMAGE. */
 #include "rs.h"
 #include "msgutils.h"
 #include "summary.h"
-
-#include "QCSharewareManager.h"
 
 #include "QCOleDropSource.h"
 
@@ -383,78 +417,49 @@ void CTridentReadMessageView::OnUpdateCenterAlign(CCmdUI* pCmdUI)
 
 void CTridentReadMessageView::OnIndent() 
 {
-	// Shareware: In reduced feature mode, you cannot change margins
-	if (UsingFullFeatureSet())
-	{
-		// FULL FEATURE mode
-		ExecCommand( IDM_INDENT );
-		GetDocument()->SetModifiedFlag();
-	}
+	ExecCommand( IDM_INDENT );
+	GetDocument()->SetModifiedFlag();
 }
 
 
 void CTridentReadMessageView::OnUpdateIndent(CCmdUI* pCmdUI) 
 {
-	// Shareware: In reduced feature mode, you cannot change margins
-	if (UsingFullFeatureSet())
+	// FULL FEATURE mode
+	DWORD dwStatus = 0;
+	
+	if( m_pParentFrame->GetCheck( ID_EDIT_MESSAGE ) == 0 )
 	{
-		// FULL FEATURE mode
-		DWORD dwStatus = 0;
-		
-		if( m_pParentFrame->GetCheck( ID_EDIT_MESSAGE ) == 0 )
-		{
-			// we're not in editing mode
-			pCmdUI->Enable( FALSE );
-			return;
-		}
+		// we're not in editing mode
+		pCmdUI->Enable( FALSE );
+		return;
+	}
  
-		dwStatus = GetCommandStatus( IDM_INDENT );
-		pCmdUI->Enable( (dwStatus & OLECMDF_ENABLED) == OLECMDF_ENABLED );
-	}
-	else
-	{
-		// REDUCED FEATURE mode
-		pCmdUI->Enable(FALSE);
-	}
+	dwStatus = GetCommandStatus( IDM_INDENT );
+	pCmdUI->Enable( (dwStatus & OLECMDF_ENABLED) == OLECMDF_ENABLED );
 }
 
 
 
 void CTridentReadMessageView::OnOutdent() 
 {
-	// Shareware: In reduced feature mode, you cannot change margins
-	if (UsingFullFeatureSet())
-	{
-		// FULL FEATURE mode
-		ExecCommand( IDM_OUTDENT );
-		GetDocument()->SetModifiedFlag();
-	}
+	ExecCommand( IDM_OUTDENT );
+	GetDocument()->SetModifiedFlag();
 }
 
 
 void CTridentReadMessageView::OnUpdateOutdent(CCmdUI* pCmdUI) 
 {
-	// Shareware: In reduced feature mode, you cannot change margins
-	if (UsingFullFeatureSet())
-	{
-		// FULL FEATURE mode
-		DWORD dwStatus = 0;
+	DWORD dwStatus = 0;
 		
-		if( m_pParentFrame->GetCheck( ID_EDIT_MESSAGE ) == 0 )
-		{
-			// we're not in editing mode
-			pCmdUI->Enable( FALSE );
-			return;
-		}
-
-		dwStatus = GetCommandStatus( IDM_OUTDENT );
-		pCmdUI->Enable( (dwStatus & OLECMDF_ENABLED) == OLECMDF_ENABLED );
-	}
-	else
+	if( m_pParentFrame->GetCheck( ID_EDIT_MESSAGE ) == 0 )
 	{
-		// REDUCED FEATURE mode
-		pCmdUI->Enable(FALSE);
+		// we're not in editing mode
+		pCmdUI->Enable( FALSE );
+		return;
 	}
+
+	dwStatus = GetCommandStatus( IDM_OUTDENT );
+	pCmdUI->Enable( (dwStatus & OLECMDF_ENABLED) == OLECMDF_ENABLED );
 }
 
 
@@ -2516,39 +2521,24 @@ CTridentReadMessageView::WriteHeaders(
 
 void CTridentReadMessageView::OnBlkfmtBullettedList() 
 {
-	// Shareware: In reduced feature mode, you cannot insert bullet lists
-	if (UsingFullFeatureSet())
-	{
-		// FULL FEATURE mode
-		ExecCommand( IDM_UNORDERLIST, NULL, NULL, TRUE );
-		GetDocument()->SetModifiedFlag();
-	}
+	ExecCommand( IDM_UNORDERLIST, NULL, NULL, TRUE );
+	GetDocument()->SetModifiedFlag();
 }
 
 void CTridentReadMessageView::OnUpdateBlkfmtBullettedList(CCmdUI* pCmdUI) 
 {
-	// Shareware: In reduced feature mode, you cannot insert bullet lists
-	if (UsingFullFeatureSet())
-	{
-		// FULL FEATURE mode
-		DWORD dwStatus = 0;
+	DWORD dwStatus = 0;
 
-		if( m_pParentFrame->GetCheck( ID_EDIT_MESSAGE ) == 0 )
-		{
-			// we're not in editing mode
-			pCmdUI->Enable( FALSE );
-			return;
-		}
-
-		dwStatus = GetCommandStatus( IDM_UNORDERLIST );
-		pCmdUI->SetCheck( (dwStatus & OLECMDF_LATCHED) == OLECMDF_LATCHED );
-		pCmdUI->Enable( (dwStatus & OLECMDF_ENABLED) == OLECMDF_ENABLED );
-	}
-	else
+	if( m_pParentFrame->GetCheck( ID_EDIT_MESSAGE ) == 0 )
 	{
-		// REDUCED FEATURE mode
-		pCmdUI->Enable(FALSE);
+		// we're not in editing mode
+		pCmdUI->Enable( FALSE );
+		return;
 	}
+
+	dwStatus = GetCommandStatus( IDM_UNORDERLIST );
+	pCmdUI->SetCheck( (dwStatus & OLECMDF_LATCHED) == OLECMDF_LATCHED );
+	pCmdUI->Enable( (dwStatus & OLECMDF_ENABLED) == OLECMDF_ENABLED );
 }
 
 
@@ -2624,10 +2614,6 @@ void CTridentReadMessageView::OnUpdateBlkfmtNormal(CCmdUI* pCmdUI)
 
 void CTridentReadMessageView::OnEditInsertLink() 
 {
-	// Shareware: In reduced feature mode, you cannot insert hyperlinks
-	if (!UsingFullFeatureSet())
-		return;
-
 	// FULL FEATURE mode
 	HRESULT					hr;
 	IHTMLElement*			pElement;
@@ -2688,60 +2674,44 @@ void CTridentReadMessageView::OnEditInsertLink()
 
 void CTridentReadMessageView::OnUpdateEditInsertLink(CCmdUI* pCmdUI) 
 {
-	// Shareware: In reduced feature mode, you cannot insert hyperlinks
-	if (UsingFullFeatureSet())
+	IHTMLElement*	pElement;
+
+	if( m_pParentFrame->GetCheck( ID_EDIT_MESSAGE ) == 0 )
 	{
-		// FULL FEATURE mode
-		IHTMLElement*	pElement;
+		// we're not in editing mode
+		pCmdUI->Enable( FALSE );
+		return;
+	}
 
-		if( m_pParentFrame->GetCheck( ID_EDIT_MESSAGE ) == 0 )
-		{
-			// we're not in editing mode
-			pCmdUI->Enable( FALSE );
-			return;
-		}
+	if( HasSelectedText() )
+	{
+		pCmdUI->Enable( TRUE );
+		return;
+	}
 
-		if( HasSelectedText() )
-		{
-			pCmdUI->Enable( TRUE );
-			return;
-		}
+	// see if we're in an anchor
+	pElement = GetSelectedElement( CRString( IDS_HTML_ANCHOR ) );
 
-		// see if we're in an anchor
-		pElement = GetSelectedElement( CRString( IDS_HTML_ANCHOR ) );
-
-		if( pElement != NULL )
-		{
-			pCmdUI->Enable( TRUE );
-			pElement->Release();
-		}
-		else
-		{
-			pCmdUI->Enable( FALSE );
-		}
+	if( pElement != NULL )
+	{
+		pCmdUI->Enable( TRUE );
+		pElement->Release();
 	}
 	else
 	{
-		// REDUCED FEATURE mode
-		pCmdUI->Enable(FALSE);
+		pCmdUI->Enable( FALSE );
 	}
 }
 
 void CTridentReadMessageView::OnEditInsertPicture() 
 {
-	// Shareware: In reduced feature mode, you cannot insert pictures
-	if (UsingFullFeatureSet())
-	{
-		// FULL FEATURE mode
-		ExecCommand( IDM_IMAGE, NULL, NULL, TRUE );
-		GetDocument()->SetModifiedFlag();
-	}
+	ExecCommand( IDM_IMAGE, NULL, NULL, TRUE );
+	GetDocument()->SetModifiedFlag();
 }
 
 void CTridentReadMessageView::OnUpdateEditInsertPicture(CCmdUI* pCmdUI) 
 {
-	// Shareware: In reduced feature mode, you cannot insert pictures
-	pCmdUI->Enable( UsingFullFeatureSet() && m_pParentFrame->GetCheck( ID_EDIT_MESSAGE ) );
+	pCmdUI->Enable( m_pParentFrame->GetCheck( ID_EDIT_MESSAGE ) );
 }
 
 
@@ -2925,31 +2895,19 @@ BOOL		bIncludeHeaders)
 
 void CTridentReadMessageView::OnEditInsertHr() 
 {
-	// Shareware: In reduced feature mode, you cannot insert horiz line
-	if (UsingFullFeatureSet())
-	{
-		ExecCommand( IDM_HORIZONTALLINE, NULL, NULL, TRUE );	
-		GetDocument()->SetModifiedFlag();
-	}
+	ExecCommand( IDM_HORIZONTALLINE, NULL, NULL, TRUE );	
+	GetDocument()->SetModifiedFlag();
 }
 
 void CTridentReadMessageView::OnUpdateEditInsertHr(CCmdUI* pCmdUI) 
 {
-	// Shareware: In reduced feature mode, you cannot insert horiz line
-	if (UsingFullFeatureSet())
+	if( m_pParentFrame->GetCheck( ID_EDIT_MESSAGE ) == 0 )
 	{
-		if( m_pParentFrame->GetCheck( ID_EDIT_MESSAGE ) == 0 )
-		{
-			// we're not in editing mode
-			pCmdUI->Enable( FALSE );
-			return;
-		}
-		pCmdUI->Enable( TRUE );
+		// we're not in editing mode
+		pCmdUI->Enable( FALSE );
+		return;
 	}
-	else
-	{
-		pCmdUI->Enable( FALSE ); // Disable Insert Horiz Line in REDUCED FEATURE mode
-	}
+	pCmdUI->Enable( TRUE );
 }
 
 

46f3c64406ed4105b0a05ebae80ecdb35c0afa10
prickett <kg4ygs@gmail.com>
2018-07-06 06:10:08 +0100

    Removed references to Qualcomm's Shareware Manager from StationeryWazooWnd header and c++ files.

--- Eudora71/Eudora/StationeryWazooWnd.cpp
+++ Eudora71/Eudora/StationeryWazooWnd.cpp
@@ -22,6 +22,43 @@ INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT
 NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH 
 DAMAGE. */
 
+/*
+
+HERMES MESSENGER SOFTWARE LICENSE AGREEMENT | Hermes Messenger Client Source Code
+Copyright (c) 2018, Hermes Messenger Development Team. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, 
+are permitted (subject to the limitations in the disclaimer below) provided that 
+the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list 
+of conditions and the following disclaimer.
+
+Redistributions in binary form must reproduce the above copyright notice, this 
+list of conditions and the following disclaimer in the documentation and/or 
+other materials provided with the distribution.
+
+Neither the name of Hermes Messenger nor the names of its contributors
+may be used to endorse or promote products derived from this software without 
+specific prior written permission.
+
+NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY’S PATENT RIGHTS ARE GRANTED BY THIS 
+LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+“AS IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE 
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+File revised by Jeff Prickett (kg4ygs@gmail.com) on July 6, 2018
+    Removed references to Qualcomm's Shareware Manager.
+
+*/
+
 //
 
 #include "stdafx.h"
@@ -43,7 +80,6 @@ DAMAGE. */
 #include "QCStationeryCommand.h"
 #include "QCStationeryDirector.h"
 
-#include "QCSharewareManager.h"
 
 extern QCCommandStack		g_theCommandStack;
 extern QCStationeryDirector	g_theStationeryDirector;
@@ -202,17 +238,9 @@ void CStationeryWazooWnd::OnContextMenu(CWnd* pWnd, CPoint ptScreen)
 }
 
 
-void CStationeryWazooWnd::OnUpdateNew(CCmdUI* pCmdUI)
-{
-	OnUpdateFullFeatureSet(pCmdUI);
-}
-
 void CStationeryWazooWnd::OnUpdateEdit(CCmdUI* pCmdUI)
 {
-	OnUpdateFullFeatureSet(pCmdUI);
-
-	if (UsingFullFeatureSet())
-		EnableIfSelected(pCmdUI);
+	EnableIfSelected(pCmdUI);
 }
 
 void CStationeryWazooWnd::EnableIfSelected(CCmdUI* pCmdUI)
@@ -223,19 +251,14 @@ void CStationeryWazooWnd::EnableIfSelected(CCmdUI* pCmdUI)
 
 void CStationeryWazooWnd::OnNewStationery()
 {
-	if (!UsingFullFeatureSet())
-		FeatureNotInFree();
-	else
-	{
-		CCompMessageDoc *pCompMsgDoc = NewCompDocument();
+	CCompMessageDoc *pCompMsgDoc = NewCompDocument();
 
-		if (pCompMsgDoc)
-		{
-			pCompMsgDoc->SetIsStationery();
-			//The comp doc knows where to get the title from and what format to set it in,
-			pCompMsgDoc->SetTitle("");
-			pCompMsgDoc->m_Sum->Display();
-		}
+	if (pCompMsgDoc)
+	{
+		pCompMsgDoc->SetIsStationery();
+		//The comp doc knows where to get the title from and what format to set it in,
+		pCompMsgDoc->SetTitle("");
+		pCompMsgDoc->m_Sum->Display();
 	}
 }
 
@@ -269,17 +292,12 @@ void CStationeryWazooWnd::OnNewMessageWith()
 
 void CStationeryWazooWnd::OnEdit()
 {
-	if (!UsingFullFeatureSet())
-		FeatureNotInFree();
-	else
-	{
-		QCStationeryCommand*	pCommand;
+	QCStationeryCommand*	pCommand;
 
-		pCommand = GetSelected();
+	pCommand = GetSelected();
 
-		if( pCommand != NULL )
-			pCommand->Execute( CA_OPEN );
-	}
+	if( pCommand != NULL )
+		pCommand->Execute( CA_OPEN );
 }
 
 void CStationeryWazooWnd::OnDelete()

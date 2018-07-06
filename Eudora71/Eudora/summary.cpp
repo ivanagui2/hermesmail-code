89854a1661d445bb6832f001f82a788ce65f78c6
prickett <kg4ygs@gmail.com>
2018-07-06 05:50:47 +0100

    Removed references to Qualcomm's Shareware Manager from summary.cpp.

--- Eudora71/Eudora/summary.cpp
+++ Eudora71/Eudora/summary.cpp
@@ -21,6 +21,43 @@ INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT
 NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH 
 DAMAGE. */
 
+/*
+
+HERMES MESSENGER SOFTWARE LICENSE AGREEMENT | Hermes Messenger Client Source Code
+Copyright (c) 2018, Hermes Messenger Development Team. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, 
+are permitted (subject to the limitations in the disclaimer below) provided that 
+the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list 
+of conditions and the following disclaimer.
+
+Redistributions in binary form must reproduce the above copyright notice, this 
+list of conditions and the following disclaimer in the documentation and/or 
+other materials provided with the distribution.
+
+Neither the name of Hermes Messenger nor the names of its contributors
+may be used to endorse or promote products derived from this software without 
+specific prior written permission.
+
+NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY’S PATENT RIGHTS ARE GRANTED BY THIS 
+LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+“AS IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE 
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+File revised by Jeff Prickett (kg4ygs@gmail.com) on July 6, 2018
+    Removed references to Qualcomm's Shareware Manager.
+
+*/
+
 //
 
 #include "stdafx.h"
@@ -63,7 +100,6 @@ DAMAGE. */
 #include "AutoCompleteSearcher.h"
 
 #include "trnslate.h"
-#include "QCSharewareManager.h"
 #include "SearchManager.h"
 
 // IMAP4
@@ -1664,8 +1700,7 @@ CSummary::ComposeMessage(
 			pCompMsgDoc->m_Sum->SetPriority(in_nPriority);
 		}
 
-		// Shareware: Only allow FCC in FULL FEATURE version.
-		if ( in_pOriginatingTOC && UsingFullFeatureSet() )
+		if ( in_pOriginatingTOC )
 		{
 			bool bNonSystemToc = false;
 			if (in_pOriginatingTOC->IsImapToc())
@@ -2065,21 +2100,18 @@ int CSummary::Build(JJFile* in, BOOL Rebuild)
 	unsigned long lBodyMoodOptions=0;
 	BOOL bInSubject = FALSE;
 
-	if (UsingFullFeatureSet())
+	//IDS_INI_MOOD_MAIL_CHECK is for outgoing 
+/*	if ( (bDoIncomgMoodCheck = GetIniShort(IDS_INI_MOOD_MAIL_INCOMING)&& !IsComp())
+		  || (GetIniShort(IDS_INI_MOOD_MAIL_CHECK) && IsComp() && Rebuild)) */
+	if (Rebuild)
 	{
-		//IDS_INI_MOOD_MAIL_CHECK is for outgoing 
-/*		if ( (bDoIncomgMoodCheck = GetIniShort(IDS_INI_MOOD_MAIL_INCOMING)&& !IsComp())
-			  || (GetIniShort(IDS_INI_MOOD_MAIL_CHECK) && IsComp() && Rebuild)) */
-		if (Rebuild)
-		{
-			bDoMoodCheck = 0;
-		}
-		else if (bDoIncomgMoodCheck = GetIniShort(IDS_INI_MOOD_MAIL_INCOMING)&& 
-			GetIniShort( IDS_INI_MOOD_MAIL_CHECK ) && !IsComp())
-		{
-			if(moodwatch.Init())
-				bDoMoodCheck = 1;
-		}
+		bDoMoodCheck = 0;
+	}
+	else if (bDoIncomgMoodCheck = GetIniShort(IDS_INI_MOOD_MAIL_INCOMING)&& 
+		GetIniShort( IDS_INI_MOOD_MAIL_CHECK ) && !IsComp())
+	{
+		if(moodwatch.Init())
+			bDoMoodCheck = 1;
 	}
 
 	while (SUCCEEDED(in->GetLine(sBuffer, sizeof(sBuffer), &lNumBytesRead)) && (lNumBytesRead > 0))

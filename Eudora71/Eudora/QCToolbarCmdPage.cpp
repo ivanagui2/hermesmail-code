ab48fe39b1336f60d3d0906e910c1b87b062720c
prickett <kg4ygs@gmail.com>
2018-07-09 04:21:01 +0100

    Removed references to the Stingray Toolkit in QCToolbarCmdPage.cpp and replaced them with references to the Hermes UI Toolkit.

--- Eudora71/Eudora/QCToolbarCmdPage.cpp
+++ Eudora71/Eudora/QCToolbarCmdPage.cpp
@@ -1,3 +1,59 @@
+/* Copyright (c) 2016, Computer History Museum 
+All rights reserved. 
+Redistribution and use in source and binary forms, with or without modification, are permitted (subject to 
+the limitations in the disclaimer below) provided that the following conditions are met: 
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer. 
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following 
+   disclaimer in the documentation and/or other materials provided with the distribution. 
+ * Neither the name of Computer History Museum nor the names of its contributors may be used to endorse or promote products 
+   derived from this software without specific prior written permission. 
+NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE 
+COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
+HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH 
+DAMAGE. */
+
+/*
+
+HERMES MESSENGER SOFTWARE LICENSE AGREEMENT | Hermes Messenger Client Source Code
+Copyright (c) 2018, Hermes Messenger Development Team. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, 
+are permitted (subject to the limitations in the disclaimer below) provided that 
+the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list 
+of conditions and the following disclaimer.
+
+Redistributions in binary form must reproduce the above copyright notice, this 
+list of conditions and the following disclaimer in the documentation and/or 
+other materials provided with the distribution.
+
+Neither the name of Hermes Messenger nor the names of its contributors
+may be used to endorse or promote products derived from this software without 
+specific prior written permission.
+
+NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY’S PATENT RIGHTS ARE GRANTED BY THIS 
+LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+“AS IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE 
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+File revised by Jeff Prickett (kg4ygs@gmail.com) on July 8, 2018
+    Removed references to the Stingray Toolkit and replaced them with references
+    to the Hermes UI Toolkit.
+
+*/    
+
 // QCToolBarCmdPage.cpp : implementation file
 //
 
@@ -10,9 +66,9 @@
 /////////////////////////////////////////////////////////////////////////////
 // QCToolBarCmdPage property page
 
-IMPLEMENT_DYNAMIC(QCToolBarCmdPage, SECToolBarCmdPage)
+IMPLEMENT_DYNAMIC(QCToolBarCmdPage, HRMToolBarCmdPage)
 
-QCToolBarCmdPage::QCToolBarCmdPage( UINT uID, UINT uTitleID ) : SECToolBarCmdPage( uID, uTitleID )
+QCToolBarCmdPage::QCToolBarCmdPage( UINT uID, UINT uTitleID ) : HRMToolBarCmdPage( uID, uTitleID )
 {
 	//{{AFX_DATA_INIT(QCToolBarCmdPage)
 		// NOTE: the ClassWizard will add member initialization here
@@ -25,14 +81,14 @@ QCToolBarCmdPage::~QCToolBarCmdPage()
 
 void QCToolBarCmdPage::DoDataExchange(CDataExchange* pDX)
 {
-	SECToolBarCmdPage::DoDataExchange(pDX);
+	HRMToolBarCmdPage::DoDataExchange(pDX);
 	//{{AFX_DATA_MAP(QCToolBarCmdPage)
 		// NOTE: the ClassWizard will add DDX and DDV calls here
 	//}}AFX_DATA_MAP
 }
 
 
-BEGIN_MESSAGE_MAP(QCToolBarCmdPage, SECToolBarCmdPage)
+BEGIN_MESSAGE_MAP(QCToolBarCmdPage, HRMToolBarCmdPage)
 	//{{AFX_MSG_MAP(QCToolBarCmdPage)
 	//}}AFX_MSG_MAP
 END_MESSAGE_MAP()
@@ -53,15 +109,15 @@ BOOL QCToolBarCmdPage::OnInitDialog()
 	CRect rect = wp.rcNormalPosition;
 	int x = 6;			// in DBU
 	int y = 8;
-	SECConvertDialogUnitsToPixels(*(GetFont()), x, y);
+	HRMConvertDialogUnitsToPixels(*(GetFont()), x, y);
 	rect.DeflateRect(x, y);
 
 	int nID = AFX_IDW_TOOLBAR + 0xff;
 	for (int i = 0; i < m_btnGroups.GetSize(); i++, nID--)
 	{
 		// Create and initialise the toolbar
-		SECBtnGroup* pGroup = (SECBtnGroup*)m_btnGroups[i];
-		SECCustomizeToolBar* pToolBar = DEBUG_NEW QCCustomizeToolBar;
+		HRMBtnGroup* pGroup = (HRMBtnGroup*)m_btnGroups[i];
+		HRMCustomizeToolBar* pToolBar = DEBUG_NEW QCCustomizeToolBar;
 		pGroup->pToolBar = pToolBar;
 
 		DWORD dwStyle = CBRS_BORDER_3D | CBRS_SIZE_DYNAMIC | CBRS_FLOATING;
@@ -106,7 +162,7 @@ BOOL QCToolBarCmdPage::OnSetActive()
 {
 	INT iPos;
 	QCCustomizeToolBar*	pBar;
-	SECBtnGroup*		pBtnGroup;
+	HRMBtnGroup*		pBtnGroup;
 
 	if(m_pManager)
 	{
@@ -122,7 +178,7 @@ BOOL QCToolBarCmdPage::OnSetActive()
 		// for the same reason as below, we need to update any bitmap changes		
 		for( iPos = 0; iPos < m_btnGroups.GetSize(); iPos ++ )
 		{
-			pBtnGroup = ( SECBtnGroup* ) m_btnGroups[ iPos ];		
+			pBtnGroup = ( HRMBtnGroup* ) m_btnGroups[ iPos ];		
 			pBar = ( QCCustomizeToolBar* ) ( pBtnGroup->pToolBar );		
 			if( pBar->IsKindOf( RUNTIME_CLASS( QCCustomizeToolBar ) ) )
 			{
@@ -141,7 +197,7 @@ BOOL QCToolBarCmdPage::OnSetActive()
 	int nIndex = pList->GetCurSel();
 	if(nIndex != LB_ERR)
 	{
-		((SECBtnGroup*) m_btnGroups[nIndex])->pToolBar->EnableConfigMode(TRUE);
+		((HRMBtnGroup*) m_btnGroups[nIndex])->pToolBar->EnableConfigMode(TRUE);
 		m_nCurSel = nIndex;
 	}
 #endif

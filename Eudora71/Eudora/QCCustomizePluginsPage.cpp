a4e4bd4c2e31eb1c663923ba3cafe19115b2aa09
prickett <kg4ygs@gmail.com>
2018-07-09 05:40:30 +0100

    Replaced references to the Stingray Toolkit in QCCustomizePluginsPage with references to the Hermes UI Toolkit.

--- Eudora71/Eudora/QCCustomizePluginsPage.cpp
+++ Eudora71/Eudora/QCCustomizePluginsPage.cpp
@@ -19,6 +19,44 @@ INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT
 NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH 
 DAMAGE. */
 
+/*
+
+HERMES MESSENGER SOFTWARE LICENSE AGREEMENT | Hermes Messenger Client Source Code
+Copyright (c) 2018, Hermes Messenger Development Team. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, 
+are permitted (subject to the limitations in the disclaimer below) provided that 
+the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list 
+of conditions and the following disclaimer.
+
+Redistributions in binary form must reproduce the above copyright notice, this 
+list of conditions and the following disclaimer in the documentation and/or 
+other materials provided with the distribution.
+
+Neither the name of Hermes Messenger nor the names of its contributors
+may be used to endorse or promote products derived from this software without 
+specific prior written permission.
+
+NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY’S PATENT RIGHTS ARE GRANTED BY THIS 
+LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+“AS IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE 
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+File revised by Jeff Prickett (kg4ygs@gmail.com) on July 9, 2018
+    Removed references to the Stingray Toolkit and replaced them with references 
+    to the Hermes UI Toolkit.
+
+*/    
+
 //
 
 #include "stdafx.h"
@@ -159,7 +197,7 @@ void QCCustomizePluginsPage::SetDescription(int nID)
 		}
 	}
 	
-	SECToolBarCmdPage::SetDescription(nID);
+	HRMToolBarCmdPage::SetDescription(nID);
 }
 
 void QCCustomizePluginsPage::OnSelChanged(
@@ -198,7 +236,7 @@ LRESULT*	pResult)
 	// toolbar
 	for( iIndex = 0; iIndex < m_btnGroups.GetSize(); iIndex ++)
 	{
-		if( pCommand->GetName() == ((SECBtnGroup*) m_btnGroups[iIndex])->lpszTitle )
+		if( pCommand->GetName() == ((HRMBtnGroup*) m_btnGroups[iIndex])->lpszTitle )
 		{
 			break;
 		}
@@ -213,14 +251,14 @@ LRESULT*	pResult)
 
 	if(m_nCurSel != LB_ERR)
 	{
-		((SECBtnGroup*) 
+		((HRMBtnGroup*) 
 			m_btnGroups[m_nCurSel])->pToolBar->EnableConfigMode(FALSE);
 		m_nCurSel = LB_ERR;
 	}
 
 	for(int i=0; i<m_btnGroups.GetSize(); i++)
 	{
-		SECCustomizeToolBar* pBar = ((SECBtnGroup*) m_btnGroups[i])->pToolBar;
+		HRMCustomizeToolBar* pBar = ((HRMBtnGroup*) m_btnGroups[i])->pToolBar;
 		if(i == iIndex)
 		{
 			pBar->EnableConfigMode(TRUE);

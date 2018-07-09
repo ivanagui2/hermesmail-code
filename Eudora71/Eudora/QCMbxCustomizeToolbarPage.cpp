492bda47a255b7bab052af9f4bfe289c44f99dc2
prickett <kg4ygs@gmail.com>
2018-07-09 04:31:59 +0100

    Removed references to the Stingray Toolkit in QCMbxCustomizeToolbarPage.cpp and replaced them with references to the Hermes UI Toolkit.

--- Eudora71/Eudora/QCMbxCustomizeToolbarPage.cpp
+++ Eudora71/Eudora/QCMbxCustomizeToolbarPage.cpp
@@ -1,3 +1,59 @@
+/* Copyright (c) 2016, Computer History Museum 
+All rights reserved. 
+Redistribution and use in source and binary forms, with or without modification, are permitted (subject to 
+the limitations in the disclaimer below) provided that the following conditions are met: 
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer. 
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following 
+   disclaimer in the documentation and/or other materials provided with the distribution. 
+ * Neither the name of Computer History Museum nor the names of its contributors may be used to endorse or promote products 
+   derived from this software without specific prior written permission. 
+NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE 
+COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
+HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH 
+DAMAGE. */
+
+/*
+
+HERMES MESSENGER SOFTWARE LICENSE AGREEMENT | Hermes Messenger Client Source Code
+Copyright (c) 2018, Hermes Messenger Development Team. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, 
+are permitted (subject to the limitations in the disclaimer below) provided that 
+the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list 
+of conditions and the following disclaimer.
+
+Redistributions in binary form must reproduce the above copyright notice, this 
+list of conditions and the following disclaimer in the documentation and/or 
+other materials provided with the distribution.
+
+Neither the name of Hermes Messenger nor the names of its contributors
+may be used to endorse or promote products derived from this software without 
+specific prior written permission.
+
+NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY’S PATENT RIGHTS ARE GRANTED BY THIS 
+LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+“AS IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE 
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+File revised by Jeff Prickett (kg4ygs@gmail.com) on July 8, 2018
+    Removed references to the Stingray Toolkit and replace them with references
+    to the Hermes UI Toolkit.
+
+*/    
+
 // QCMbxCustomizeToolbarPage.cpp : implementation file
 //
 
@@ -159,7 +215,7 @@ LRESULT*	pResult)
 	HTREEITEM						hItem;
 	CTreeCtrl*						pTree;
 	CString							szName;
-	SECCustomToolBar*				pToolBar;
+	HRMCustomToolBar*				pToolBar;
 	UINT							nStyle;
 
 	*pResult = 0;
@@ -182,7 +238,7 @@ LRESULT*	pResult)
 
 	if(m_nCurSel != LB_ERR)
 	{
-		pToolBar = ((SECBtnGroup*) m_btnGroups[m_nCurSel])->pToolBar;
+		pToolBar = ((HRMBtnGroup*) m_btnGroups[m_nCurSel])->pToolBar;
 		
 		pToolBar->EnableConfigMode(FALSE);
 
@@ -224,7 +280,7 @@ LRESULT*	pResult)
 	// toolbar
 	for( iIndex = 0; iIndex < m_btnGroups.GetSize(); iIndex ++)
 	{
-		if( szName == ((SECBtnGroup*) m_btnGroups[iIndex])->lpszTitle )
+		if( szName == ((HRMBtnGroup*) m_btnGroups[iIndex])->lpszTitle )
 		{
 			break;
 		}
@@ -238,7 +294,7 @@ LRESULT*	pResult)
 
 	for(int i=0; i<m_btnGroups.GetSize(); i++)
 	{
-		SECCustomizeToolBar* pBar = ((SECBtnGroup*) m_btnGroups[i])->pToolBar;
+		HRMCustomizeToolBar* pBar = ((HRMBtnGroup*) m_btnGroups[i])->pToolBar;
 		if(i == iIndex)
 		{
 			pBar->EnableConfigMode(TRUE);

d07c9c60433a65aa12883e50413bd5830e205960
prickett <kg4ygs@gmail.com>
2018-07-10 05:23:51 +0100

    Removed references to the Stingray Toolkit in EmoticonToolbarButton.cpp and replaced them with references to the Hermes UI Toolkit.

--- Eudora71/Eudora/EmoticonToolbarButton.cpp
+++ Eudora71/Eudora/EmoticonToolbarButton.cpp
@@ -20,6 +20,44 @@ INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT
 NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH 
 DAMAGE. */
 
+/*
+
+HERMES MESSENGER SOFTWARE LICENSE AGREEMENT | Hermes Messenger Client Source Code
+Copyright (c) 2018, Hermes Messenger Development Team. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, 
+are permitted (subject to the limitations in the disclaimer below) provided that 
+the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list 
+of conditions and the following disclaimer.
+
+Redistributions in binary form must reproduce the above copyright notice, this 
+list of conditions and the following disclaimer in the documentation and/or 
+other materials provided with the distribution.
+
+Neither the name of Hermes Messenger nor the names of its contributors
+may be used to endorse or promote products derived from this software without 
+specific prior written permission.
+
+NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY’S PATENT RIGHTS ARE GRANTED BY THIS 
+LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+“AS IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE 
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+File revised by Jeff Prickett (kg4ygs@gmail.com) on July 10, 2018
+    Removed references to the Stingray Toolkit and replaced them with references
+    to the Hermes UI Toolkit.
+
+*/    
+
 //
 ////////////////////////////////////////////////////////////////////////
 
@@ -45,12 +83,12 @@ IMPLEMENT_BUTTON(EmoticonToolbarButton)
 
 
 EmoticonToolbarButton::EmoticonToolbarButton()
-	:	SECTwoPartBtn(), m_pLastEmoticonDrawn(NULL)
+	:	HRMTwoPartBtn(), m_pLastEmoticonDrawn(NULL)
 {
 }
 
 
-void EmoticonToolbarButton::DrawButton(CDC& dc, SECBtnDrawData& data)
+void EmoticonToolbarButton::DrawButton(CDC& dc, HRMBtnDrawData& data)
 {
 	// We want the emoticon toolbar button to reflect the most recently selected
 	// emoticon rather than the default icon.
@@ -60,9 +98,9 @@ void EmoticonToolbarButton::DrawButton(CDC& dc, SECBtnDrawData& data)
 	// been written (handling normal state, pressed state, and disabled
 	// drawing).
 	//
-	// Instead, we'll be sneaky. SECTwoPartBtn already knows how to draw
+	// Instead, we'll be sneaky. HRMTwoPartBtn already knows how to draw
 	// a normal button where the icon doesn't change. All we need to do
-	// is add the smarts to change the icon behind its back. SECTwoPartBtn
+	// is add the smarts to change the icon behind its back. HRMTwoPartBtn
 	// (and its parent classes) will do all the hard work of drawing and
 	// be none the wiser.
 	Emoticon *	pEmoticon = GetCurrentEmoticon();
@@ -88,7 +126,7 @@ void EmoticonToolbarButton::DrawButton(CDC& dc, SECBtnDrawData& data)
 			int		nIconY = 0;
 			
 			// Erase the previous icon
-			extern SEC_AUX_DATA		secData;
+			extern HRM_AUX_DATA		secData;
 			
 			data.m_bmpDC.FillSolidRect(nIconX, nIconY, m_pToolBar->GetImgWidth(), m_pToolBar->GetImgHeight(), secData.clrBtnFace);
 			
@@ -97,7 +135,7 @@ void EmoticonToolbarButton::DrawButton(CDC& dc, SECBtnDrawData& data)
 		}
 	}
 	
-	SECTwoPartBtn::DrawButton(dc, data);
+	HRMTwoPartBtn::DrawButton(dc, data);
 }
 
 
@@ -108,9 +146,9 @@ void EmoticonToolbarButton::DrawButton(CDC& dc, SECBtnDrawData& data)
 ////////////////////////////////////////////////////////////////////////
 BOOL EmoticonToolbarButton::BtnPressDown(CPoint point)
 {
-	BOOL bRet = SECTwoPartBtn::BtnPressDown(point);
+	BOOL bRet = HRMTwoPartBtn::BtnPressDown(point);
 	
-	if (bRet && (m_nStyle & SEC_TBBS_PRESSED2))
+	if (bRet && (m_nStyle & HRM_TBBS_PRESSED2))
 	{
 		// Draw the right button with the pressed style
 		Invalidate();
@@ -149,7 +187,7 @@ BOOL EmoticonToolbarButton::BtnPressDown(CPoint point)
 
 		// Get rid of the pressed style and redraw the right button so it displays normally
 		// and the left button gets redrawn with the new color.
-		m_nStyle &= ~(SEC_TBBS_PRESSED2 | SEC_TBBS_TRACKING2);
+		m_nStyle &= ~(HRM_TBBS_PRESSED2 | HRM_TBBS_TRACKING2);
 		Invalidate();
 
 		// Act like nothing ever happened

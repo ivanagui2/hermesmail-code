3d8b56c0a2bbff90f9ee3c9a42923a4d8c294eee
prickett <kg4ygs@gmail.com>
2018-07-17 23:15:55 +0100

    Removed references to the Stingray Toolkit in DockBar.cpp and replaced them with references to the Hermes UI Toolkit.

--- Eudora71/Eudora/DockBar.cpp
+++ Eudora71/Eudora/DockBar.cpp
@@ -19,6 +19,44 @@ INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT
 NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH 
 DAMAGE. */
 
+/*
+
+HERMES MESSENGER SOFTWARE LICENSE AGREEMENT | Hermes Messenger Client Source Code
+Copyright (c) 2018, Hermes Messenger Development Team. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, 
+are permitted (subject to the limitations in the disclaimer below) provided that 
+the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list 
+of conditions and the following disclaimer.
+
+Redistributions in binary form must reproduce the above copyright notice, this 
+list of conditions and the following disclaimer in the documentation and/or 
+other materials provided with the distribution.
+
+Neither the name of Hermes Messenger nor the names of its contributors
+may be used to endorse or promote products derived from this software without 
+specific prior written permission.
+
+NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY’S PATENT RIGHTS ARE GRANTED BY THIS 
+LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+“AS IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE 
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+File revised by Jeff Prickett (kg4ygs@gmail.com) on July 17, 2017
+    Removed references to the Stingray Toolkit and replaced them with references 
+    to the Hermes UI Toolkit.
+ 
+*/    
+
 //
 
 #include "stdafx.h"
@@ -43,7 +81,7 @@ QCDockBar::~QCDockBar()
 //
 // Returns in what column and row a given control bar is located.
 /////////////////////////////////////////////////////////////////////////////
-bool QCDockBar::FindControlBarLocation(SECControlBar* in_pBar, int & out_nCol, int & out_nRow)
+bool QCDockBar::FindControlBarLocation(HRMControlBar* in_pBar, int & out_nCol, int & out_nRow)
 {
 	bool	bFound = false;
 
@@ -78,7 +116,7 @@ bool QCDockBar::FindControlBarLocation(SECControlBar* in_pBar, int & out_nCol, i
 
 
 
-BEGIN_MESSAGE_MAP(QCDockBar, SECDockBar)
+BEGIN_MESSAGE_MAP(QCDockBar, HRMDockBar)
 	//{{AFX_MSG_MAP(QCDockBar)
 		// NOTE - the ClassWizard will add and remove mapping macros here.
 	//}}AFX_MSG_MAP

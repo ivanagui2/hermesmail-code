15c4848ec634e7783e1d4ece496b49fa8e446ecd
prickett <kg4ygs@gmail.com>
2018-07-17 23:56:20 +0100

    Removed references to the Stingray Toolkit from ColorToolbarButton.cpp and replaced them with references to the Hermes UI Toolkit.

--- Eudora71/Eudora/ColorToolbarButton.cpp
+++ Eudora71/Eudora/ColorToolbarButton.cpp
@@ -20,6 +20,44 @@ INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT
 NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH 
 DAMAGE. */
 
+/*
+
+HERMES MESSENGER SOFTWARE LICENSE AGREEMENT | Hermes Messenger Client Source Code
+Copyright (c) 2018, Hermes Messenger Development Team. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, 
+are permitted (subject to the limitations in the disclaimer below) provided that 
+the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list 
+of conditions and the following disclaimer.
+
+Redistributions in binary form must reproduce the above copyright notice, this 
+list of conditions and the following disclaimer in the documentation and/or 
+other materials provided with the distribution.
+
+Neither the name of Hermes Messenger nor the names of its contributors
+may be used to endorse or promote products derived from this software without 
+specific prior written permission.
+
+NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY’S PATENT RIGHTS ARE GRANTED BY THIS 
+LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+“AS IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE 
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+File revised by Jeff Prickett (kg4ygs@gmail.com) on July 17, 2017
+    Removed references to the Stingray Toolkit and replaced them with references
+    to the Hermes UI Toolkit.
+
+*/    
+
 //
 ////////////////////////////////////////////////////////////////////////
 
@@ -44,13 +82,13 @@ IMPLEMENT_BUTTON(CColorToolbarButton)
 ////////////////////////////////////////////////////////////////////////
 // DrawFace [protected, virtual]
 //
-// Override of SECStdBtn::DrawFace() implemenation.  Handles the details
+// Override of HRMStdBtn::DrawFace() implemenation.  Handles the details
 // of drawing the colored square on top of the standard button.
 ////////////////////////////////////////////////////////////////////////
-void CColorToolbarButton::DrawFace(SECBtnDrawData& data, BOOL bForce, int& x, int& y, 
+void CColorToolbarButton::DrawFace(HRMBtnDrawData& data, BOOL bForce, int& x, int& y, 
 						 int& nWidth, int& nHeight, int nImgWidth)
 {
-	SECTwoPartBtn::DrawFace(data, bForce, x, y, nWidth, nHeight, nImgWidth);
+	HRMTwoPartBtn::DrawFace(data, bForce, x, y, nWidth, nHeight, nImgWidth);
 
 	// Draw the colored rectangle, but only for the left side of the button.
 	// The only way you can tell the left side from the right is the width.
@@ -81,7 +119,7 @@ void CColorToolbarButton::DrawFace(SECBtnDrawData& data, BOOL bForce, int& x, in
 ////////////////////////////////////////////////////////////////////////
 BOOL CColorToolbarButton::BtnPressDown(CPoint point)
 {
-	BOOL bRet = SECTwoPartBtn::BtnPressDown(point);
+	BOOL bRet = HRMTwoPartBtn::BtnPressDown(point);
 	
 	if (bRet && (m_nStyle & SEC_TBBS_PRESSED2))
 	{
